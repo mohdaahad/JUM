1d0611dcdefee569fb7af1b540c24769e8c9b0d0
Mohd Aahad <93692744+aahadrahi@users.noreply.github.com>
2022-10-31 11:23:49 +0530

    django update

--- JUM/urls.py
+++ JUM/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import  path, include
-from django.views.static import  serve
-from django.conf.urls import  url
-from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
